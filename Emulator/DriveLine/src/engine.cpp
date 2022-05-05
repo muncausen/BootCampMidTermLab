42d2ceea50ca3ddf01b37b598fb3f5f8b390ffd1
rezazakernejad <rezazaker@gmail.com>
2022-05-05 14:42:20 +0200

    pointer to enging moved to private

--- Emulator/DriveLine/src/engine.cpp
+++ Emulator/DriveLine/src/engine.cpp
@@ -3,23 +3,15 @@
 #include "ui.hpp"
 
 void Engine::GearCalc(const uint8_t& throtle, const uint8_t& user_gear, const uint8_t& ignition) {
-  clock_t d[]{2 * CLOCKS_PER_SEC, 10 * CLOCKS_PER_SEC, 15 * CLOCKS_PER_SEC, 25 * CLOCKS_PER_SEC, 35 * CLOCKS_PER_SEC};
-  if (ignition == static_cast<uint>(Ignition::kOn)) {
-    if (user_gear > static_cast<uint>(Gear::kPark)) {
+  clock_t d[]{2 * CLOCKS_PER_SEC, 10 * CLOCKS_PER_SEC, 10 * CLOCKS_PER_SEC, 8 * CLOCKS_PER_SEC, 5 * CLOCKS_PER_SEC};
+  if (true /*ignition == static_cast<uint>(Ignition::kOn)*/) {
+    if (user_gear > static_cast<uint>(Gear::kNeutral)) {
       for (int i = 0; i < 5; i++) {
-        if (gear_ > static_cast<uint>(Gear::kNeutral)) {
-          Delay(d[i]);
-          ++gear_;
-          printf("Gear %d \n ", gear_);
-        } else {
-          printf("Gear %d \n ", gear_);
-        }
+        Delay(d[i]);
+        ++gear_;
+        printf("Auto Gear ... %d \n ", gear_);
       }
-    } else {
-      rpm_ = 800;
     }
-  } else {
-    rpm_ = 0;
   }
 }
 void Engine::Rpm(const uint8_t& throtle, const uint8_t& gear) { rpm_ = throtle * 3 / gear; }
@@ -32,6 +24,5 @@ void Delay(const clock_t& delay) {
   clock_t now = clock();
   while (clock() - now < delay / 10)
     ;
-  cout << "Message Show after delay that you entered " << clock() - now << endl;
   int main();
 }

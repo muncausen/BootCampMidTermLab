74954c14aff9d83bba57498ac0bac48d191f981a
rezazakernejad <rezazaker@gmail.com>
2022-05-05 13:06:16 +0200

    CMakeLists addressed to common folders

--- Emulator/DriveLine/server.hpp
+++ Emulator/DriveLine/server.hpp
@@ -1,24 +1,25 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
-#include<iostream>
-#include<cstring>
-#include "can_receiver.hpp"
+#include <cstring>
+#include <iostream>
+#include <ui.hpp>
+
+#include "can_tranceiver.hpp"
 #include "engine.hpp"
-#include<ui.hpp>
 using namespace std;
 
-class Server{
-        // CanFrame can_ui_frame;
-         
-    public:
-        Server() = default;
-        bool StartEmulate(const UserInputCanFrame&);
-        DisplayCanFrame can_data_to_disp;
-        uint8_t prev_cntr{0};
-        Engine* engine;
-        CanTranceiver* can_tranceiver;
-        uint8_t engine_rpm;
+class Server {
+  // CanFrame can_ui_frame;
+
+ public:
+  Server() = default;
+  bool StartEmulate(const UserInputCanFrame&);
+  DisplayCanFrame can_data_to_disp;
+  uint8_t prev_cntr{0};
+  Engine* engine;
+  CanTranceiver* can_tranceiver;
+  uint8_t engine_rpm;
 };
 
 #endif
--- Emulator/test/unit/emu_gtest.hpp
+++ Emulator/test/unit/emu_gtest.hpp
@@ -1,27 +1,26 @@
 
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
-#include"can_receiver.hpp"
 #include <iostream>
-#include "server.hpp"
+
+#include "can_tranceiver.hpp"
 #include "engine.hpp"
+#include "server.hpp"
 using namespace std;
 
-class TestEmulator : public ::testing::Test
-{
-    public:
-        TestEmulator() = default;
-        void SetUp() override;
-        void TearDown() override;
-        bool UpdateCanData(UserInputCanFrame& can_ui_frame){
-            UserInputCanFrame test_cfr;
-            test_cfr.frame_counter = can_ui_frame.frame_counter;
-            cout << "TEST:: Frame_Cntr: " << test_cfr.frame_counter << endl;
-        }
-        CanTranceiver *can_receiver_ = nullptr;
-        Server* server_ = nullptr;
-        UserInputCanFrame testcfr;
-        Engine* engine = nullptr;
-        
+class TestEmulator : public ::testing::Test {
+ public:
+  TestEmulator() = default;
+  void SetUp() override;
+  void TearDown() override;
+  bool UpdateCanData(UserInputCanFrame& can_ui_frame) {
+    UserInputCanFrame test_cfr;
+    test_cfr.frame_counter = can_ui_frame.frame_counter;
+    cout << "TEST:: Frame_Cntr: " << test_cfr.frame_counter << endl;
+  }
+  CanTranceiver* can_receiver_ = nullptr;
+  Server* server_ = nullptr;
+  UserInputCanFrame testcfr;
+  Engine* engine = nullptr;
 };

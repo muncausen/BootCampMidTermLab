6fb89f059418837ef8c921c7c2f6b17c820c4a08
Viktor Södergren <viktor.sodergren@volvocars.com>
2022-05-05 21:18:29 +0200

    Make UI thread safe and Cmd() without parameters
    - Added mutex and lock when updating bitfield and copying info to CAN.
    - Removed unnecessary parameter in Cmd() function.
    - Updated input unit tests accordingly.

--- UserInput/src/main.cpp
+++ UserInput/src/main.cpp
@@ -10,15 +10,19 @@ int main() {
   // Initiate persistent variables.
   bool run{true};
   ui::UserInput ui{};
+  std::mutex mx{};
 
   ui::InitNcurses();
 
-  std::thread can_thread(ui::SendToCan, std::ref(ui));
+  std::thread can_thread(ui::SendToCan, std::ref(ui), std::ref(mx));
 
   // Will loop until false is returned from input check.
   while (run) {
     if ((ui.ch = getch()) != ERR) {
-      run = ui.Cmd(ui.ch);
+      run = ui.Cmd();
+
+      // Lock mutex and update.
+      std::lock_guard<std::mutex> lk(mx);
       ui.UpdateCanFrameBitfield();
     }
   }

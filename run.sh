373255167b601cdffcc71fac4576063d7c123806
Nikolay Zverkhovskiy <30926854+muncausen@users.noreply.github.com>
2022-05-17 16:07:19 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gnome-terminal --title=Display -- ~/BootCampMidTermLab/build/Display/display -c vcan0
-# gnome-terminal --title=CANdump -- candump vcan0
-gnome-terminal --title=Simulator -- ~/BootCampMidTermLab/build/Emulator/simulator
-gnome-terminal --title=Input -- ~/BootCampMidTermLab/build/UserInput/ui
